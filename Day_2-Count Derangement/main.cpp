a1b3caee350f2c48a85e562b032d782d98dd42e5
Mohit Dubey <md7985751@gmail.com>
2026-04-20 19:13:21 +0530

    Day-2 solution

---
+++ Day_2-Count Derangement/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem link : https://www.geeksforgeeks.org/problems/dearrangement-of-balls0918/1
+ */
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int fact(int num)
+    {
+        if (num <= 1)
+            return 1;
+        return num * fact(num - 1);
+    }
+    int derangeCount(int n)
+    {
+        int ans = 0;
+        for (int i = 0; i <= n; i++)
+        {
+            ans += fact(n) * (pow(-1, i) / fact(i));
+        }
+        return ans;
+    }
+};
