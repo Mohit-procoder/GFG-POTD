f0e481fe1321c348e47b7d602bb70b5dbc17745b
Mohit Dubey <md7985751@gmail.com>
2026-04-21 18:23:36 +0530

    added comments for better understandings

--- Day_3-Two Water Jug Problem/main.cpp
+++ Day_3-Two Water Jug Problem/main.cpp
@@ -5,12 +5,13 @@
 #include<bits/stdc++.h>
 class Solution {
   public:
-  
+  // calcultae GCD (or use inbuilt function)
    int gcd(int a , int b){
         if( b==0) return a;
         return gcd( b , a%b);
    }
    
+   // actual logic for the solution 
     int fun( int n , int m , int d){
          int i = n ,  j =0;
           int ans =1;
@@ -31,8 +32,8 @@ class Solution {
           }
            return ans;
     }
+    // main function to call 
     int minSteps(int m, int n, int d) {
-        // Code here
         if( d > max( n , m)) return -1;
         if(d%gcd( n , m) !=0) return -1;
         return min( fun( n , m ,d) , fun( m , n, d));
