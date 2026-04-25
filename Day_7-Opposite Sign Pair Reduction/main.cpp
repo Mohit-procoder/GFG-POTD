34b68cd502bda2283b46860e508d164b31b9a989
Mohit Dubey <md7985751@gmail.com>
2026-04-25 14:35:59 +0530

    day 6 solution

---
+++ Day_7-Opposite Sign Pair Reduction/main.cpp
@@ -0,0 +1,22 @@
+// problem : https://www.geeksforgeeks.org/problems/asteroid-collision/1
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    vector<int> reducePairs(vector<int>& arr) {
+        stack<int>s;
+        for(auto i:arr){
+            while(!s.empty()&&((s.top()<0&&i>0)||(s.top()>0&&i<0))&&(abs(s.top())<abs(i)))s.pop();
+            if(s.empty())s.push(i);
+            else if(((s.top()<0&&i>0)||(s.top()>0&&i<0))&&(abs(s.top())==abs(i)))s.pop();
+            else if(((s.top()<0&&i<0)||(s.top()>0&&i>0)))s.push(i);
+        }
+        vector<int>ans;
+        while(!s.empty()){
+            ans.push_back(s.top());
+            s.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
