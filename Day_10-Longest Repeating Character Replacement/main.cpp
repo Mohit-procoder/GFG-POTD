3df7dec8959a16cec7c542ab62c7f1ea72b5f89b
Mohit Dubey <md7985751@gmail.com>
2026-04-28 16:30:46 +0530

    Day-10 solution

---
+++ Day_10-Longest Repeating Character Replacement/main.cpp
@@ -0,0 +1,22 @@
+// Problem : https://www.geeksforgeeks.org/problems/longest-repeating-character-replacement/1
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int longestSubstr(string& s, int k) {
+        int ans=0,t=k;
+        for(char c='A';c<='Z';c++){
+            int i=0,j=0,k=t;
+            while(j<s.size()){
+                if(s[j]!=c)k--;j++;
+                while(k<0){
+                    if(s[i]!=c)k++;
+                    i++;
+                }
+                ans=max(ans, j-i);
+            }
+        }
+        return ans;
+    }
+};
