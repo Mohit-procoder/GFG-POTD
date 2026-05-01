c02f482725f360afe14232a0882a3b016138543f
Mohit Dubey <md7985751@gmail.com>
2026-05-01 11:19:30 +0530

    Day next commit okay

---
+++ Day_13-Kth Largest Element/main.cpp
@@ -0,0 +1,20 @@
+// problem : https://www.geeksforgeeks.org/problems/kth-largest-element-in-a-stream2220/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> kthLargest(vector<int>& arr, int k) {
+        vector<int>ans(arr.size(),-1);
+        priority_queue<int, vector<int>, greater<int>>p;
+        for(int i=0;i<k;i++)p.push(arr[i]);
+        ans[k-1]=p.top();
+        for(int i=k;i<arr.size();i++){
+            p.push(arr[i]);
+            p.pop();
+            ans[i]=p.top();
+        }
+        return ans;
+    }
+};
