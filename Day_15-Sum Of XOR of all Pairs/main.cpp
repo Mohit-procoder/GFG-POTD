41140729a8905756d1a7a316376b06ebcdfedbb1
Mohit Dubey <md7985751@gmail.com>
2026-05-05 19:36:54 +0530

    day 15 solution added

---
+++ Day_15-Sum Of XOR of all Pairs/main.cpp
@@ -0,0 +1,28 @@
+// problem : https://www.geeksforgeeks.org/problems/sum-of-xor-of-all-pairs0723/1
+
+// solution :
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    long long sumXOR(vector<int> &arr) {
+        long long ans = 0;
+        int n = arr.size();
+        int vec[32]{0};
+        for(auto i: arr){
+            int j = 0;
+            while(i){
+                if(i&1)vec[j]++;
+                i = i>>1;
+                j++;
+            }
+        }
+        long long t = 1;
+        for(int i =0  ;i<32 ; i++){
+            ans+= t * (vec[i]) * (n- vec[i]);
+            t = t<<1;
+        }
+        return ans;
+    }
+};
