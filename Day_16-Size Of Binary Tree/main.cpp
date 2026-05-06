25f8bf720b5fcc95288a533b9b94525d467f160f
Mohit Dubey <md7985751@gmail.com>
2026-05-06 20:55:09 +0530

    fixed a small error

--- Day_16-Size Of Binary Tree/main.cpp
+++ Day_16-Size Of Binary Tree/main.cpp
@@ -5,6 +5,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
 class Solution {
   public:
     int getSize(Node* root) {
