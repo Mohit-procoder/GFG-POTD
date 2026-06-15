7064d1ae4010f26522b8ee6a1864152097634b21
Mohit Dubey <md7985751@gmail.com>
2026-06-15 09:43:19 +0530

    added solution for day 29

--- Day_29-Minimum cost to fill given weight in a bag/main.cpp
+++ Day_29-Minimum cost to fill given weight in a bag/main.cpp
@@ -5,3 +5,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Solution {
+  public:
+    int minimumCost(vector<int> &cost, int w) {
+        vector<int>dp(w+1,INT_MAX);dp[0]=0;
+        for(int i=0;i<cost.size();i++)
+            if(cost[i]==-1)continue;
+            else for(int j=1;j<=w;j++)
+                   if(j>=i+1&&dp[j-i-1]!=INT_MAX)dp[j]=min(dp[j],dp[j-i-1]+cost[i]);
+        if(dp[w]==INT_MAX)return -1;return dp[w];
+    }
+};
