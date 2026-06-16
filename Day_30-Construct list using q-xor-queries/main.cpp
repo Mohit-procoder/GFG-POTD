e39a85185df11525a2f88abe438011c74c036ee4
Mohit Dubey <md7985751@gmail.com>
2026-06-16 10:27:35 +0530

    added problem statement

---
+++ Day_30-Construct list using q-xor-queries/main.cpp
@@ -0,0 +1,26 @@
+// problem : https://www.geeksforgeeks.org/problems/construct-list-using-given-q-xor-queries/1
+
+// solution :
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> constructList(vector<vector<int>> &q)
+    {
+        reverse(q.begin(), q.end());
+        vector<int> ans;
+        ans.push_back(0);
+        int t = 0;
+        for (int i = 0; i < q.size(); i++)
+            if (q[i][0] == 0)
+                ans.push_back(q[i][1] ^ t);
+            else
+                t ^= q[i][1];
+        ans[0] ^= t;
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
