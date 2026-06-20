8c5f03ee5cf45201d936ac47dfdb8741c8914ce0
Mohit Dubey <md7985751@gmail.com>
2026-06-20 08:39:01 +0530

    Added boiler code for day 33

---
+++ Day_33-Find Last Digit Of ab Fro Large Number/main.cpp
@@ -0,0 +1,10 @@
+// problem : https://www.geeksforgeeks.org/problems/find-last-digit-of-ab-for-large-numbers1936/1
+
+// solution :
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+}
