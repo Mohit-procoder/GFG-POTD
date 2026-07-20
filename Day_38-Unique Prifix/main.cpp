0ba161a50d7b2033b9f0ab5ebbb41c0342e077b5
Mohit Dubey <md7985751@gmail.com>
2026-07-20 13:16:39 +0530

    added day 38 solution

---
+++ Day_38-Unique Prifix/main.cpp
@@ -0,0 +1,37 @@
+// problem : https://www.geeksforgeeks.org/problems/shortest-unique-prefix-for-every-word/1
+
+// solution:
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<string> findPrefixes(vector<string>& arr) {
+        unordered_map<string, int> mp;
+        vector<string> ans;
+
+        for (string &s : arr) {
+            string prefix = "";
+            for (char ch : s) {
+                prefix += ch;
+                mp[prefix]++;
+            }
+        }
+        for (string &s : arr) {
+            string prefix = "";
+            int flag=0;
+            for (char ch : s) {
+                prefix += ch;
+                if (mp[prefix] == 1) {
+                    ans.push_back(prefix);
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0){
+                ans.push_back(s);
+            }
+        }
+
+        return ans;
+    }
+};
